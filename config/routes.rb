a6d0c887723a4366f097b44c10de0013e97a6a1f
Darlene Koon <dkoon3@roadrunner.com>
2015-08-20 10:55:54 -0400

    Add devise authetication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   resources :clubs
+  
   resources :clubs
+  
   resources :pets
 
   resources :owners
 
   resources :clubs
+
+  root 'clubs#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
